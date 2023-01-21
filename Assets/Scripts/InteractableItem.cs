4494f24b04f6d491553aa0b36d8c3c7f60684eb5
Ronald <kpn.nl.rs@gmail.com>
2023-01-21 15:38:21 +0100

    Preliminary support for item pickup

--- Assets/Scripts/InteractableItem.cs
+++ Assets/Scripts/InteractableItem.cs
@@ -6,13 +6,22 @@ using UnityEngine;
 public class InteractableItem : MonoBehaviour
 {
     public GameObject PopupUIGameObject;
+    public string NarrotorSoundname = "en_us:germangenades";
 
+    public GameObject ActivatorObject = null;
+    //TODO: Somehow get a held object
+    public GameObject AbstractHeldObject = null;
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<PlayerTag>() != null)
         {
+            if (AbstractHeldObject == null ||
+                AbstractHeldObject != ActivatorObject)
+                return;
+            
             PopupUIGameObject.SetActive(true);
-            AudioManager.Instance.Play("en_us:germangenades");
+            AudioManager.Instance.Play(NarrotorSoundname);
         }
     }
 }

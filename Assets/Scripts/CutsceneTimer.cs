d599da9650a74bfaac3731b424df2f90339f3d71
Ronald <kpn.nl.rs@gmail.com>
2023-01-22 22:16:38 +0100

    Added timer for custscene

---
+++ Assets/Scripts/CutsceneTimer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+ public class CutsceneTimer : MonoBehaviour
+ {
+     public float Finish = 60f;
+     public GameObject PlayerCamera;
+     public GameObject CutsceneCamera;
+     public GameObject CustsceneTimeline;
+
+     private float currentTime = 0f;
+
+     private void Update()
+     {
+            currentTime += Time.deltaTime;
+    
+            if (currentTime >= Finish)
+            {
+                PlayerCamera.SetActive(false);
+                CutsceneCamera.SetActive(true);
+                CustsceneTimeline.SetActive(true);
+            }
+     }
+ }
